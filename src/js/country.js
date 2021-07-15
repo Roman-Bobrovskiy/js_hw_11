ccd3ac5bf4cab295c94f616bee0e3d8a771e7c8e
RB <bobrovskiyroman@gmail.com>
2021-07-15 16:13:07 +0300

    if

--- src/js/country.js
+++ src/js/country.js
@@ -26,13 +26,31 @@ class Country {
       });
   }
   renderData = arrData => {
-    let dataCollection = arrData.map(({ name }) => {
-      let p = document.createElement('p');
+    if (arrData.length > 2 && arrData.length < 10) {
+      let dataCollection = arrData.map(({ name }) => {
+        let p = document.createElement('p');
+        p.textContent = name;
+        return p;
+      });
+      this.list.append(...dataCollection);
+    }
+    if (arrData.length > 10) {
+      alert('More than 10');
+    }
+    if (arrData.length === 1) {
+      arrData.map(({ name, capital, population, languages, flag }) => {
+        let objLanguages = languages.map(elem => elem.name);
 
-      p.textContent = name;
-      return p;
-    });
-    this.list.append(...dataCollection);
+        this.list.insertAdjacentHTML(
+          'beforeend',
+          `<li class ="flag"> <img class="gallery__image" src="${flag}"  alt=""/></li>
+             <li class="text"> Country: ${name}</li>
+             <li class="text"> Capital: ${capital}</li>
+             <li class="text"> Population: ${population}</li>
+             <li class="text"> Languages: ${objLanguages}</li>`,
+        );
+      });
+    }
   };
 }
 let _ = require('lodash');
