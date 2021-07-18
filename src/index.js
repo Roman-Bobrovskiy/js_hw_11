0b29ea2d016203afa30ed045be54cfca65077341
RB <bobrovskiyroman@gmail.com>
2021-07-18 19:13:07 +0300

    rev 1.3

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './styles.css';
 import './js/country.js';
+import '@pnotify/core/dist/Angeler.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/Material.css';
+import '@pnotify/core/dist/PNotify.css';
